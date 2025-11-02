ee55a7fdc6b1695e3796a097d244708892d3a78a
MuhammadBilal561 <bilalrehan2006@gmail.com>
2025-11-02 10:33:19 +0500

    get orders

--- Order/OrderRepoDB.cs
+++ Order/OrderRepoDB.cs
@@ -80,6 +80,45 @@ namespace ShopManagementSystem.Order
             }
         }
 
-       
+        public List<OrderModel> GetAll()
+        {
+            List<OrderModel> orders = new List<OrderModel>();
+
+            string query = "SELECT * FROM OrderDetail ORDER BY OrderDate DESC";
+
+            using (SqlConnection con = new SqlConnection(Utils.DBConnection()))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["OrderID"]);
+                    string name = reader["CustomerName"].ToString();
+                    string phone = reader["PhoneNo"].ToString();
+                    int age = Convert.ToInt32(reader["Age"]);
+                    string address = reader["Address"].ToString();
+                    DateTime date = Convert.ToDateTime(reader["OrderDate"]);
+                    string itemsText = reader["ItemsText"].ToString();
+                    double total = Convert.ToDouble(reader["TotalAmount"]);
+
+                    OrderModel order = new OrderModel(name, phone, age, address);
+                    order.SetOrderDate(date);
+                    order.SetTotalAmount(total);
+
+                    List<OrderItemModel> items = ConvertStringToItems(itemsText);
+                    foreach (OrderItemModel item in items)
+                    {
+                        order.AddItems(item);
+                    }
+
+                    orders.Add(order);
+                }
+                reader.Close();
+            }
+
+            return orders;
+        }
     }
 }
